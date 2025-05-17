4923f0ce7ed0a37df9ccfba31b6dca181c62f72c
Aggron282 <marcokhodr1@gmail.com>
2025-05-17 01:42:49 -0700

    S

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,16 @@ var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+
+
+app.use(cors({
+  origin: ["http://localhost:3000","https://scrapsquad-883401bf1df6.herokuapp.com/"],  // Your React frontend
+  credentials: true
+}));
+
+app.set('trust proxy', 1);
 
 app.listen(port,()=>{
   console.log("App is runnig on localhost:"+port);
