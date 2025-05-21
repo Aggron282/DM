002d0ed3b4b129be8ab5fdd0c3d57f71d085799b
Aggron282 <marcokhodr1@gmail.com>
2025-05-21 02:07:00 -0700

    s

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -6,61 +6,45 @@ import { useEffect } from 'react';
 
 function Navbar() {
 
-  useEffect(() => {
-    const stickyNavbar = document.getElementById('stickyNavbar');
-    const mainNavbar = document.getElementById('mainNavbar');
 
-    const handleScroll = () => {
-      const offset = mainNavbar.offsetHeight;
-      if (window.scrollY > offset) {
-        stickyNavbar.classList.add('show');
-      } else {
-        stickyNavbar.classList.remove('show');
-      }
-    };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
 
   return (
     <>
       {/* Discount Banner */}
       {/* Top Utility Bar */}
-  <div className="top-nav">
-    <div className="top-left">
-      <span>Residential</span>
-      <span>Commercial</span>
-    </div>
-    <div className="top-right">
-      <input type="text" placeholder="Search" className="search-box" />
-      <a href="/login">Log In</a>
-      <a href="/billing">Pay My Bill</a>
-      <select className="lang-switcher">
-        <option>US/EN</option>
-        <option>ES</option>
-      </select>
-    </div>
-  </div>
+      {/* Top Utility Bar */}
+    <div className="top-nav">
+      <div className="top-left">
+      <a href="/login">Junk Removal Services</a>
 
-  {/* Main Nav */}
-  <nav className="navbar" id="mainNavbar">
-    <div className="nav-left">
-      <a href="/" className="logo-link">
-        <img src={Logo} alt="Company Logo" className="logo" />
-      </a>
+      </div>
+      <div className="top-right">
+        <a href="/login">info@scrapsquad.com</a>
+        <a href="/billing">480-309-3030 </a>
+        <a href="/login">Review Us</a>
+      </div>
     </div>
-    <ul className="nav-links">
-      <li><a href="/services">Services</a></li>
-      <li><a href="/dumping-101">Dumping 101</a></li>
-      <li><a href="/about">About</a></li>
-      <li><a href="/contact">Support</a></li>
-    </ul>
-    <div className="nav-right">
-      <button className="btn-outline">Branch Finder</button>
-      <a href="/pricing" className="btn-cta">See Pricing</a>
-    </div>
-  </nav>
+
+    {/* Main Navigation Bar */}
+    <nav className="navbar" id="mainNavbar">
+      <div className="nav-left">
+        <a href="/" className="logo-link">
+          <img src={Logo} alt="Scrap Squad Logo" className="logo" />
+        </a>
+      </div>
+      <span class="title">Scrap Squad </span>
+
+      <ul className="nav-links">
+        <li><a href="/services">Services</a></li>
+        <li><a href="/dumping-101">Dumping 101</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/support">Support</a></li>
+      </ul>
+      <div className="nav-right">
+        <a href="/pricing" className="btn-cta">See Pricing</a>
+      </div>
+    </nav>
 
 
     </>
